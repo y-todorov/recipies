273cb39e9992e9f3422db7fcaa4a390f62620cea
y-todorov <ytodorov@ytodorov.com>
2013-08-16 10:42:57 +0300

    removed some references from web project

--- RecipiesSite/RecipiesWebFormApp/Account/RegisterExternalLogin.aspx.cs
+++ RecipiesSite/RecipiesWebFormApp/Account/RegisterExternalLogin.aspx.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Web;
 using System.Web.Security;
-using DotNetOpenAuth.AspNet;
 using Microsoft.AspNet.Membership.OpenAuth;
 
 namespace RecipiesWebFormApp.Account
@@ -52,63 +51,63 @@ namespace RecipiesWebFormApp.Account
 
         private void ProcessProviderResult()
         {
-            // Process the result from an auth provider in the request
-            ProviderName = OpenAuth.GetProviderNameFromCurrentRequest();
+            //// Process the result from an auth provider in the request
+            //ProviderName = OpenAuth.GetProviderNameFromCurrentRequest();
 
-            if (String.IsNullOrEmpty(ProviderName))
-            {
-                Response.Redirect(FormsAuthentication.LoginUrl);
-            }
+            //if (String.IsNullOrEmpty(ProviderName))
+            //{
+            //    Response.Redirect(FormsAuthentication.LoginUrl);
+            //}
 
-            // Build the redirect url for OpenAuth verification
-            var redirectUrl = "~/Account/RegisterExternalLogin";
-            var returnUrl = Request.QueryString["ReturnUrl"];
-            if (!String.IsNullOrEmpty(returnUrl))
-            {
-                redirectUrl += "?ReturnUrl=" + HttpUtility.UrlEncode(returnUrl);
-            }
+            //// Build the redirect url for OpenAuth verification
+            //var redirectUrl = "~/Account/RegisterExternalLogin";
+            //var returnUrl = Request.QueryString["ReturnUrl"];
+            //if (!String.IsNullOrEmpty(returnUrl))
+            //{
+            //    redirectUrl += "?ReturnUrl=" + HttpUtility.UrlEncode(returnUrl);
+            //}
 
-            // Verify the OpenAuth payload
-            var authResult = OpenAuth.VerifyAuthentication(redirectUrl);
-            ProviderDisplayName = OpenAuth.GetProviderDisplayName(ProviderName);
-            if (!authResult.IsSuccessful)
-            {
-                Title = "External login failed";
-                userNameForm.Visible = false;
+            //// Verify the OpenAuth payload
+            //var authResult = OpenAuth.VerifyAuthentication(redirectUrl);
+            //ProviderDisplayName = OpenAuth.GetProviderDisplayName(ProviderName);
+            //if (!authResult.IsSuccessful)
+            //{
+            //    Title = "External login failed";
+            //    userNameForm.Visible = false;
 
-                ModelState.AddModelError("Provider", String.Format("External login {0} failed.", ProviderDisplayName));
+            //    ModelState.AddModelError("Provider", String.Format("External login {0} failed.", ProviderDisplayName));
 
-                // To view this error, enable page tracing in web.config (<system.web><trace enabled="true"/></system.web>) and visit ~/Trace.axd
-                Trace.Warn("OpenAuth", String.Format("There was an error verifying authentication with {0})", ProviderDisplayName), authResult.Error);
-                return;
-            }
+            //    // To view this error, enable page tracing in web.config (<system.web><trace enabled="true"/></system.web>) and visit ~/Trace.axd
+            //    Trace.Warn("OpenAuth", String.Format("There was an error verifying authentication with {0})", ProviderDisplayName), authResult.Error);
+            //    return;
+            //}
 
-            // User has logged in with provider successfully
-            // Check if user is already registered locally
-            if (OpenAuth.Login(authResult.Provider, authResult.ProviderUserId, createPersistentCookie: false))
-            {
-                RedirectToReturnUrl();
-            }
+            //// User has logged in with provider successfully
+            //// Check if user is already registered locally
+            //if (OpenAuth.Login(authResult.Provider, authResult.ProviderUserId, createPersistentCookie: false))
+            //{
+            //    RedirectToReturnUrl();
+            //}
 
-            // Store the provider details in ViewState
-            ProviderName = authResult.Provider;
-            ProviderUserId = authResult.ProviderUserId;
-            ProviderUserName = authResult.UserName;
+            //// Store the provider details in ViewState
+            //ProviderName = authResult.Provider;
+            //ProviderUserId = authResult.ProviderUserId;
+            //ProviderUserName = authResult.UserName;
 
-            // Strip the query string from action
-            Form.Action = ResolveUrl(redirectUrl);
+            //// Strip the query string from action
+            //Form.Action = ResolveUrl(redirectUrl);
 
-            if (User.Identity.IsAuthenticated)
-            {
-                // User is already authenticated, add the external login and redirect to return url
-                OpenAuth.AddAccountToExistingUser(ProviderName, ProviderUserId, ProviderUserName, User.Identity.Name);
-                RedirectToReturnUrl();
-            }
-            else
-            {
-                // User is new, ask for their desired membership name
-                userName.Text = authResult.UserName;
-            }
+            //if (User.Identity.IsAuthenticated)
+            //{
+            //    // User is already authenticated, add the external login and redirect to return url
+            //    OpenAuth.AddAccountToExistingUser(ProviderName, ProviderUserId, ProviderUserName, User.Identity.Name);
+            //    RedirectToReturnUrl();
+            //}
+            //else
+            //{
+            //    // User is new, ask for their desired membership name
+            //    userName.Text = authResult.UserName;
+            //}
         }
 
         private void CreateAndLoginUser()
