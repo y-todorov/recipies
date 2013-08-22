dab81d8708970399a6f0c6948a17d746eef37c1b
y-todorov <ytodorov@ytodorov.com>
2013-08-22 23:03:14 +0300

    removed radajax becaouse rad grid export problem

--- RecipiesSite/RecipiesWebFormApp/Site.Master.designer.cs
+++ RecipiesSite/RecipiesWebFormApp/Site.Master.designer.cs
@@ -58,15 +58,6 @@ namespace RecipiesWebFormApp {
         protected global::Telerik.Web.UI.RadAjaxLoadingPanel RadAjaxLoadingPanel1;
         
         /// <summary>
-        /// RadAjaxPanel1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::Telerik.Web.UI.RadAjaxPanel RadAjaxPanel1;
-        
-        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
