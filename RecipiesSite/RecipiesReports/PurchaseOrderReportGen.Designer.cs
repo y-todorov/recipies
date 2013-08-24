856d2bc597a8ea926f2d02696e546d7bb5b20cd0
y-todorov <ytodorov@ytodorov.com>
2013-08-24 22:53:18 +0300

    added purchase order report

---
+++ RecipiesSite/RecipiesReports/PurchaseOrderReportGen.Designer.cs
@@ -0,0 +1,111 @@
+namespace RecipiesReports
+{
+    partial class PurchaseOrderReportGen
+    {
+        #region Component Designer generated code
+        /// <summary>
+        /// Required method for telerik Reporting designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Telerik.Reporting.Drawing.StyleRule styleRule1 = new Telerik.Reporting.Drawing.StyleRule();
+            Telerik.Reporting.Drawing.StyleRule styleRule2 = new Telerik.Reporting.Drawing.StyleRule();
+            Telerik.Reporting.Drawing.StyleRule styleRule3 = new Telerik.Reporting.Drawing.StyleRule();
+            Telerik.Reporting.Drawing.DescendantSelector descendantSelector1 = new Telerik.Reporting.Drawing.DescendantSelector();
+            Telerik.Reporting.Drawing.StyleRule styleRule4 = new Telerik.Reporting.Drawing.StyleRule();
+            Telerik.Reporting.Drawing.DescendantSelector descendantSelector2 = new Telerik.Reporting.Drawing.DescendantSelector();
+            this.pageHeaderSection1 = new Telerik.Reporting.PageHeaderSection();
+            this.detail = new Telerik.Reporting.DetailSection();
+            this.pageFooterSection1 = new Telerik.Reporting.PageFooterSection();
+            this.openAccessDataSource1 = new Telerik.Reporting.OpenAccessDataSource();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            // 
+            // pageHeaderSection1
+            // 
+            this.pageHeaderSection1.Height = Telerik.Reporting.Drawing.Unit.Inch(2D);
+            this.pageHeaderSection1.Name = "pageHeaderSection1";
+            // 
+            // detail
+            // 
+            this.detail.Height = Telerik.Reporting.Drawing.Unit.Inch(2D);
+            this.detail.Name = "detail";
+            // 
+            // pageFooterSection1
+            // 
+            this.pageFooterSection1.Height = Telerik.Reporting.Drawing.Unit.Inch(2D);
+            this.pageFooterSection1.Name = "pageFooterSection1";
+            // 
+            // openAccessDataSource1
+            // 
+            this.openAccessDataSource1.ConnectionString = "Data Source=804b3a50-697f-4e00-b7cc-a1e6015cf402.sqlserver.sequelizer.com;Persist" +
+    " Security Info=True;User ID=uehevqpfoljtplku;Password=uhffcctREdsiocbJhjETidnG5q" +
+    "5jWtB25oyDasHUYHNVtA8EzVMz5NPr4FiTgi3k";
+            this.openAccessDataSource1.Name = "openAccessDataSource1";
+            this.openAccessDataSource1.ObjectContext = "DynamicApplicationModel.RecipiesModel, DynamicApplicationModel, Version=1.0.0.0, " +
+    "Culture=neutral, PublicKeyToken=null";
+            this.openAccessDataSource1.ObjectContextMember = "PurchaseOrderDetails";
+            this.openAccessDataSource1.ProviderName = "System.Data.SqlClient";
+            // 
+            // PurchaseOrderReportGen
+            // 
+            this.DataSource = this.openAccessDataSource1;
+            this.Items.AddRange(new Telerik.Reporting.ReportItemBase[] {
+            this.pageHeaderSection1,
+            this.detail,
+            this.pageFooterSection1});
+            this.Name = "PurchaseOrderReportGen";
+            this.PageSettings.Margins = new Telerik.Reporting.Drawing.MarginsU(Telerik.Reporting.Drawing.Unit.Inch(1D), Telerik.Reporting.Drawing.Unit.Inch(1D), Telerik.Reporting.Drawing.Unit.Inch(1D), Telerik.Reporting.Drawing.Unit.Inch(1D));
+            this.PageSettings.PaperKind = System.Drawing.Printing.PaperKind.Letter;
+            this.Style.BackgroundColor = System.Drawing.Color.White;
+            styleRule1.Selectors.AddRange(new Telerik.Reporting.Drawing.ISelector[] {
+            new Telerik.Reporting.Drawing.TypeSelector(typeof(Telerik.Reporting.TextItemBase)),
+            new Telerik.Reporting.Drawing.TypeSelector(typeof(Telerik.Reporting.HtmlTextBox))});
+            styleRule1.Style.Padding.Left = Telerik.Reporting.Drawing.Unit.Point(2D);
+            styleRule1.Style.Padding.Right = Telerik.Reporting.Drawing.Unit.Point(2D);
+            styleRule2.Selectors.AddRange(new Telerik.Reporting.Drawing.ISelector[] {
+            new Telerik.Reporting.Drawing.StyleSelector(typeof(Telerik.Reporting.Table), "Normal.TableNormal")});
+            styleRule2.Style.BorderColor.Default = System.Drawing.Color.Black;
+            styleRule2.Style.BorderStyle.Default = Telerik.Reporting.Drawing.BorderType.Solid;
+            styleRule2.Style.BorderWidth.Default = Telerik.Reporting.Drawing.Unit.Pixel(1D);
+            styleRule2.Style.Color = System.Drawing.Color.Black;
+            styleRule2.Style.Font.Name = "Tahoma";
+            styleRule2.Style.Font.Size = Telerik.Reporting.Drawing.Unit.Point(9D);
+            descendantSelector1.Selectors.AddRange(new Telerik.Reporting.Drawing.ISelector[] {
+            new Telerik.Reporting.Drawing.TypeSelector(typeof(Telerik.Reporting.Table)),
+            new Telerik.Reporting.Drawing.StyleSelector(typeof(Telerik.Reporting.ReportItem), "Normal.TableHeader")});
+            styleRule3.Selectors.AddRange(new Telerik.Reporting.Drawing.ISelector[] {
+            descendantSelector1});
+            styleRule3.Style.BorderColor.Default = System.Drawing.Color.Black;
+            styleRule3.Style.BorderStyle.Default = Telerik.Reporting.Drawing.BorderType.Solid;
+            styleRule3.Style.BorderWidth.Default = Telerik.Reporting.Drawing.Unit.Pixel(1D);
+            styleRule3.Style.Font.Name = "Tahoma";
+            styleRule3.Style.Font.Size = Telerik.Reporting.Drawing.Unit.Point(10D);
+            styleRule3.Style.VerticalAlign = Telerik.Reporting.Drawing.VerticalAlign.Middle;
+            descendantSelector2.Selectors.AddRange(new Telerik.Reporting.Drawing.ISelector[] {
+            new Telerik.Reporting.Drawing.TypeSelector(typeof(Telerik.Reporting.Table)),
+            new Telerik.Reporting.Drawing.StyleSelector(typeof(Telerik.Reporting.ReportItem), "Normal.TableBody")});
+            styleRule4.Selectors.AddRange(new Telerik.Reporting.Drawing.ISelector[] {
+            descendantSelector2});
+            styleRule4.Style.BorderColor.Default = System.Drawing.Color.Black;
+            styleRule4.Style.BorderStyle.Default = Telerik.Reporting.Drawing.BorderType.Solid;
+            styleRule4.Style.BorderWidth.Default = Telerik.Reporting.Drawing.Unit.Pixel(1D);
+            styleRule4.Style.Font.Name = "Tahoma";
+            styleRule4.Style.Font.Size = Telerik.Reporting.Drawing.Unit.Point(9D);
+            this.StyleSheet.AddRange(new Telerik.Reporting.Drawing.StyleRule[] {
+            styleRule1,
+            styleRule2,
+            styleRule3,
+            styleRule4});
+            this.Width = Telerik.Reporting.Drawing.Unit.Inch(6D);
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
+        #endregion
+
+        private Telerik.Reporting.PageHeaderSection pageHeaderSection1;
+        private Telerik.Reporting.DetailSection detail;
+        private Telerik.Reporting.PageFooterSection pageFooterSection1;
+        private Telerik.Reporting.OpenAccessDataSource openAccessDataSource1;
+    }
+}
