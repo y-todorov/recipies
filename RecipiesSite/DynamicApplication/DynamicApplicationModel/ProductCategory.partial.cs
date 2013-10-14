73affdd26bd4549c211cf55aa7a134dd4f9e63c8
y-todorov <ytodorov@ytodorov.com>
2013-10-14 23:49:48 +0300

    no errors in solution

--- RecipiesSite/DynamicApplication/DynamicApplicationModel/ProductCategory.partial.cs
+++ RecipiesSite/DynamicApplication/DynamicApplicationModel/ProductCategory.partial.cs
@@ -2,22 +2,6 @@
 {
     public partial class ProductCategory : YordanBaseEntity
     {
-        //public override void BeforeInsert(RecipiesModel context)
-        //{
-        //    ProductCategory inserted = this;
-        //    base.BeforeInsert(context);
-        //}
-
-        //public override void BeforeUpdate(RecipiesModel context)
-        //{
-        //    ProductCategory updated = this;
-        //    base.BeforeUpdate(context);
-        //}
-
-        //public override void BeforeDelete(RecipiesModel context)
-        //{
-        //    ProductCategory deleted = this;
-        //    base.BeforeDelete(context);
-        //}
+       
     }
 }
