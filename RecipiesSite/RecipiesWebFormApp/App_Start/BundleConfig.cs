9668a871a24b0b5737685d4d7184fa82e6b76c00
y-todorov <ytodorov@ytodorov.com>
2013-11-09 00:21:42 +0200

    test for bundles of js files

--- RecipiesSite/RecipiesWebFormApp/App_Start/BundleConfig.cs
+++ RecipiesSite/RecipiesWebFormApp/App_Start/BundleConfig.cs
@@ -2,11 +2,48 @@
 
 namespace RecipiesWebFormApp
 {
+
+    // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254726
     public class BundleConfig
     {
-        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254726
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            
+            bundles.UseCdn = true;
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr", "http://ajax.aspnetcdn.com/ajax/modernizr/modernizr-2.0.6-development-only.js"));//.Include(
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery", "http://cdn.kendostatic.com/2013.2.918/js/jquery.min.js"));//.Include(
+            //"~/Scripts/jquery-{version}.js"));
+
+            //bundles.Add(new ScriptBundle("~/bundles/kendo", "http://cdn.kendostatic.com/2013.2.918/js/jquery.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/kendoall", "http://cdn.kendostatic.com/2013.2.918/js/kendo.all.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/kendoaspnetmvc", "http://cdn.kendostatic.com/2013.2.918/js/kendo.aspnetmvc.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/kendoculture", "http://cdn.kendostatic.com/2013.2.918/js/cultures/kendo.culture.en-IE.min.js"));
+
+
+
+            BundleTable.EnableOptimizations = true;
+
+            
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr", "http://ajax.aspnetcdn.com/ajax/modernizr/modernizr-2.0.6-development-only.js"));//.Include(
+                        //"~/Scripts/modernizr-*"));
+
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //          "~/Scripts/bootstrap.js",
+            //          "~/Scripts/respond.js"));
+
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/Content/bootstrap.css",
+            //          "~/Content/site.css"));
         }
     }
+
 }
