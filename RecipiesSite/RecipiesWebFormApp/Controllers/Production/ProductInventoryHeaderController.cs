2272a1ad51f649aba7bdfc7a5f7fa9d3a61de359
y-todorov <ytodorov@ytodorov.com>
2013-11-13 01:06:36 +0200

    remove foo code.

--- RecipiesSite/RecipiesWebFormApp/Controllers/Production/ProductInventoryHeaderController.cs
+++ RecipiesSite/RecipiesWebFormApp/Controllers/Production/ProductInventoryHeaderController.cs
@@ -15,39 +15,6 @@ namespace InventoryManagementMVC.Controllers
     {
         public ActionResult Index()
         {
-            List<ProductInventory> inv1 = ContextFactory.Current.Inventories.OfType<ProductInventory>().Where(p => p.ForDate == new DateTime(2013, 9, 9)).ToList();
-            foreach (var item in inv1)
-            {
-                item.ProductInventoryHeaderId = 1;
-                ContextFactory.Current.SaveChanges();
-            }
-
-            List<ProductInventory>  inv2 = ContextFactory.Current.Inventories.OfType<ProductInventory>().Where(p => p.ForDate == new DateTime(2013, 9, 22)).ToList();
-            foreach (var item in inv2)
-            {
-                item.ProductInventoryHeaderId = 2;
-                ContextFactory.Current.SaveChanges();
-            }
-
-            List<ProductInventory>  inv3 = ContextFactory.Current.Inventories.OfType<ProductInventory>().Where(p => p.ForDate == new DateTime(2013, 10, 20)).ToList();
-            foreach (var item in inv3)
-            {
-                item.ProductInventoryHeaderId = 3;
-                ContextFactory.Current.SaveChanges();
-            }
-
-            List<ProductInventory> inv4 = ContextFactory.Current.Inventories.OfType<ProductInventory>().Where(p => p.ForDate == new DateTime(2013, 10, 21)).ToList();
-            foreach (var item in inv4)
-            {
-                item.ProductInventoryHeaderId = 4;
-                ContextFactory.Current.SaveChanges();
-            }
-
-            ContextFactory.Current.SaveChanges();
-
-            //ContextFactory.Current.SaveChanges();
-
-
             List<ProductInventoryHeaderViewModel> productInventoriesViewModels =
                 ContextFactory.Current.ProductInventoryHeaders.ToList().Select
                     (pi =>
