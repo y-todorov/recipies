5a6b88db798e0c671445a0b18952c29e5f8b936a
y-todorov <ytodorov@ytodorov.com>
2013-11-21 09:43:47 +0200

    crud sales order details

---
+++ RecipiesSite/RecipiesWebFormApp/Controllers/Sales/SalesOrderDetailController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using InventoryManagementMVC.Models;
+using InventoryManagementMVC.Models.Purchasing;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using RecipiesModelNS;
+using System.Data.Entity;
+using DevTrends.MvcDonutCaching; // .Include !!!!!!! THIS IS SO IMPROTANT
+
+namespace InventoryManagementMVC.Controllers
+{
+    public class SalesOrderDetailController : ControllerBase
+    {
+        public ActionResult Read(int? salesOrderHeaderId, [DataSourceRequest] DataSourceRequest request)
+        {
+            List<SalesOrderDetailViewModel> salesOrderDetailViewModels =
+                ContextFactory.Current.SalesOrderDetails.Where(
+                    pod => salesOrderHeaderId.HasValue ? pod.SalesOrderHeaderId == salesOrderHeaderId.Value : true)
+                    .ToList().Select
+                    (pod =>
+                        SalesOrderDetailViewModel.ConvertFromSalesOrderDetailEntity(pod,
+                            new SalesOrderDetailViewModel())).ToList();
+            return Json(salesOrderDetailViewModels.ToDataSourceResult(request));
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Create(int? salesOrderHeaderId, [DataSourceRequest] DataSourceRequest request,
+            [Bind(Prefix = "models")] IEnumerable<SalesOrderDetailViewModel> salesOrderDetails)
+        {
+            if (salesOrderDetails != null && ModelState.IsValid)
+            {
+                foreach (SalesOrderDetailViewModel sodViewModel in salesOrderDetails)
+                {
+                    sodViewModel.SalesOrderHeaderId = salesOrderHeaderId;
+                    SalesOrderDetail newPodEntity =
+                        SalesOrderDetailViewModel.ConvertToSalesOrderDetailEntity(sodViewModel, new SalesOrderDetail());
+                    ContextFactory.Current.SalesOrderDetails.Add(newPodEntity);
+                    ContextFactory.Current.SaveChanges();
+                    // Prefetch Product and others ...
+                    SalesOrderDetailViewModel.ConvertFromSalesOrderDetailEntity(newPodEntity, sodViewModel);
+                }
+            }
+
+            return Json(salesOrderDetails.ToDataSourceResult(request, ModelState));
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Update([DataSourceRequest] DataSourceRequest request,
+            [Bind(Prefix = "models")] IEnumerable<SalesOrderDetailViewModel> salesOrderDetails)
+        {
+            if (salesOrderDetails != null && ModelState.IsValid)
+            {
+                foreach (SalesOrderDetailViewModel sodViewModel in salesOrderDetails)
+                {
+                    SalesOrderDetail pohEntity =
+                        ContextFactory.Current.SalesOrderDetails.FirstOrDefault(
+                            c => c.SalesOrderDetailId == sodViewModel.SalesOrderDetailId);
+
+                    SalesOrderDetailViewModel.ConvertToSalesOrderDetailEntity(sodViewModel, pohEntity);
+
+                    ContextFactory.Current.SaveChanges();
+                    SalesOrderDetailViewModel.ConvertFromSalesOrderDetailEntity(pohEntity, sodViewModel);
+                }
+            }
+
+            return Json(salesOrderDetails.ToDataSourceResult(request, ModelState));
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Destroy([DataSourceRequest] DataSourceRequest request,
+            [Bind(Prefix = "models")] IEnumerable<SalesOrderDetailViewModel> salesOrderDetails)
+        {
+            foreach (SalesOrderDetailViewModel sod in salesOrderDetails)
+            {
+                SalesOrderDetail podEntity =
+                    ContextFactory.Current.SalesOrderDetails.FirstOrDefault(
+                        c => c.SalesOrderDetailId == sod.SalesOrderDetailId);
+                ContextFactory.Current.SalesOrderDetails.Remove(podEntity);
+
+                ContextFactory.Current.SaveChanges();
+            }
+
+            return Json(salesOrderDetails.ToDataSourceResult(request, ModelState));
+        }
+    }
+}
---
+++ RecipiesSite/RecipiesWebFormApp/Models/Sales/SalesOrderDetailViewModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using RecipiesModelNS;
+using System.ComponentModel;
+using InventoryManagementMVC.DataAnnotations;
+using System.Web.Mvc;
+
+namespace InventoryManagementMVC.Models
+{
+    public class SalesOrderDetailViewModel
+    {
+        [Key]
+        public int SalesOrderDetailId { get; set; }
+        
+        [ReadOnly(true)]    
+        [Display(Name = "Sales Order")]
+        [Editable(false)]
+        [HiddenInput(DisplayValue = false)]
+        public int? SalesOrderHeaderId { get; set; }
+
+        [Relation(EntityType = typeof(Recipe), DataFieldValue = "RecipeId", DataFieldText = "Name")]
+        [Display(Name = "Recipe")]
+        public int? RecipeId { get; set; }
+     
+
+        [Display(Name = "Order QTY")]
+        public double? OrderQuantity { get; set; }
+
+        public decimal? UnitPrice { get; set; }
+
+
+        public double? UnitPriceDiscount { get; set; }
+
+        [ReadOnly(true)]
+        public decimal LineTotal { get; set; }
+     
+        public DateTime? ModifiedDate { get; set; }
+
+        public string ModifiedByUser { get; set; }
+
+        public static SalesOrderDetailViewModel ConvertFromSalesOrderDetailEntity(
+            SalesOrderDetail entity, SalesOrderDetailViewModel model)
+        {
+            model.LineTotal = (decimal)entity.LineTotal;
+            model.ModifiedByUser = entity.ModifiedByUser;
+            model.ModifiedDate = entity.ModifiedDate;
+            model.OrderQuantity = entity.OrderQuantity;
+            model.RecipeId = entity.RecipeId;
+            model.SalesOrderDetailId = entity.SalesOrderDetailId;
+            model.SalesOrderHeaderId = entity.SalesOrderHeaderId;
+            model.UnitPrice = entity.UnitPrice;
+            model.UnitPriceDiscount = entity.UnitPriceDiscount;
+
+            return model;
+        }
+
+        public static SalesOrderDetail ConvertToSalesOrderDetailEntity(SalesOrderDetailViewModel model,
+            SalesOrderDetail entity)
+        {
+            entity.LineTotal = (double)model.LineTotal;
+            entity.ModifiedByUser = model.ModifiedByUser;
+            entity.ModifiedDate = model.ModifiedDate;
+            entity.OrderQuantity = model.OrderQuantity;
+            entity.RecipeId = model.RecipeId;
+            entity.SalesOrderDetailId = model.SalesOrderDetailId;
+            entity.SalesOrderHeaderId = model.SalesOrderHeaderId;
+            entity.UnitPrice = model.UnitPrice;
+            entity.UnitPriceDiscount = model.UnitPriceDiscount;
+
+            return entity;
+        }
+    }
+}
