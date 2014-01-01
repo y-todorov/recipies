1218bcf74103fb2f63147ac5000a12a8c797b085
y-todorov <ytodorov@ytodorov.com>
2014-01-01 18:44:57 +0200

    PaymentTypeController implemented all in base class

--- RecipiesSite/RecipiesWebFormApp/Controllers/Sales/PaymentTypeController.cs
+++ RecipiesSite/RecipiesWebFormApp/Controllers/Sales/PaymentTypeController.cs
@@ -1,4 +1,5 @@
-﻿using InventoryManagementMVC.Models.Purchasing;
+﻿using System.Collections;
+using InventoryManagementMVC.Models.Purchasing;
 using Kendo.Mvc.UI;
 using RecipiesModelNS;
 using System;
@@ -26,7 +27,7 @@ namespace InventoryManagementMVC.Controllers.Purchasing
 
         public ActionResult Read([DataSourceRequest] DataSourceRequest request)
         {
-            var result = base.ReadBase(request, typeof(PaymentTypeViewModel), typeof(PaymentType));
+            var result = ReadBase(request, typeof(PaymentTypeViewModel), typeof(PaymentType));
             return result;
         }
 
@@ -34,107 +35,24 @@ namespace InventoryManagementMVC.Controllers.Purchasing
         public ActionResult Create([DataSourceRequest] DataSourceRequest request,
             [Bind(Prefix = "models")] IEnumerable<PaymentTypeViewModel> paymentTypes)
         {
-            
-            if (paymentTypes != null && ModelState.IsValid)
-            {
-                foreach (PaymentTypeViewModel paymentTypeViewModel in paymentTypes)
-                {
-                    PaymentType newEntity = paymentTypeViewModel.ConvertToEntity(new PaymentType());
-                      
-                    ContextFactory.Current.PaymentTypes.Add(newEntity);
-                    ContextFactory.Current.SaveChanges();
-
-                    paymentTypeViewModel.ConvertFromEntity(newEntity);
-                    //PaymentTypeViewModel.ConvertFromPaymentTypeEntity(newEntity, paymentTypeViewModel);
-                }
-            }
-
-            return Json(paymentTypes.ToDataSourceResult(request, ModelState));
+            var result = CreateBase(request, paymentTypes, typeof(PaymentTypeViewModel), typeof(PaymentType));
+            return result;
         }
 
         [AcceptVerbs(HttpVerbs.Post)]
         public ActionResult Update([DataSourceRequest] DataSourceRequest request,
             [Bind(Prefix = "models")] IEnumerable<PaymentTypeViewModel> paymentTypes)
         {
-            if (paymentTypes != null && ModelState.IsValid)
-            {
-                foreach (PaymentTypeViewModel paymentType in paymentTypes)
-                {
-                    PaymentType entity =
-                        ContextFactory.Current.PaymentTypes.FirstOrDefault(
-                            c => c.PaymentTypeId == paymentType.PaymentTypeId);
-
-                    paymentType.ConvertToEntity(entity);
-                    //PaymentTypeViewModel.ConvertToPaymentTypeEntity(paymentType, entity);
-
-                    ContextFactory.Current.SaveChanges();
-
-                    paymentType.ConvertFromEntity(entity);
-                    //PaymentTypeViewModel.ConvertFromPaymentTypeEntity(entity, paymentType);
-                }
-            }
-
-            return Json(paymentTypes.ToDataSourceResult(request, ModelState));
+            var result = UpdateBase(request, paymentTypes, typeof(PaymentTypeViewModel), typeof(PaymentType));
+            return result;
         }
 
         [AcceptVerbs(HttpVerbs.Post)]
         public ActionResult Destroy([DataSourceRequest] DataSourceRequest request,
             [Bind(Prefix = "models")] IEnumerable<PaymentTypeViewModel> paymentTypes)
         {
-            var result = base.DestroyBase(request, paymentTypes, typeof(PaymentTypeViewModel), typeof(PaymentType));
+            var result = DestroyBase(request, paymentTypes, typeof(PaymentTypeViewModel), typeof(PaymentType));
             return result;
-
-            //Type modelType = typeof(PaymentTypeViewModel);
-            // Type entityType = typeof(PaymentType);
-
-            //DbSet dbset = ContextFactory.Current.Set(entityType);
-            //dbset.Load();
-            //var en = dbset.Local.GetEnumerator();
-
-            ////dbset.Remove()
-
-            //List<object> result = new List<object>();
-            //while (en.MoveNext())
-            //{
-            //    dynamic newModel = Activator.CreateInstance(modelType);
-            //    dynamic newEntity = Activator.CreateInstance(entityType);
-            //    newEntity = en.Current;
-            //    var modelToAdd = newModel.ConvertFromEntity(newEntity);
-            //    if (paymentTypes.Any(m => m == modelToAdd))
-            //    {
-            //        result.Add(en.Current);
-            //    }
-            //}
-
-            //foreach (var o in result)
-            //{
-            //    dbset.Remove(o);
-            //    ContextFactory.Current.SaveChanges();
-            //}
-
-
-
-            //foreach (PaymentTypeViewModel paymentType in paymentTypes)
-            //{
-            //    foreach (object pt in result)
-            //    {
-            //        if (paymentType.ConvertToEntity((PaymentType)pt) == pt)
-            //        {
-
-            //            dbset.Remove(pt);
-            //        }
-            //    }
-                
-
-            //    //PaymentType entity =
-            //    //    ContextFactory.Current.PaymentTypes.FirstOrDefault(
-            //    //        c => c.PaymentTypeId == paymentType.PaymentTypeId);
-            //    //ContextFactory.Current.PaymentTypes.Remove(entity);
-
-            //    ContextFactory.Current.SaveChanges();
-            //}
-
-            return Json(paymentTypes.ToDataSourceResult(request, ModelState));
         }
     }
 }
--- RecipiesSite/RecipiesWebFormApp/Controllers/Shared/ControllerBase.cs
+++ RecipiesSite/RecipiesWebFormApp/Controllers/Shared/ControllerBase.cs
@@ -1,7 +1,10 @@
-﻿using System.Data.Entity;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Reflection;
 using Autofac;
 using DevTrends.MvcDonutCaching;
 using DevTrends.MvcDonutCaching.Annotations;
+using InventoryManagementMVC.DataAnnotations;
 using InventoryManagementMVC.Models;
 using Kendo.Mvc.Extensions;
 using Kendo.Mvc.UI;
@@ -18,7 +21,7 @@ using System.Collections;
 
 namespace InventoryManagementMVC.Controllers
 {
-    [DonutOutputCache(Duration = 24*3600,
+    [DonutOutputCache(Duration = 24 * 3600,
         Options = OutputCacheOptions.IgnoreFormData | OutputCacheOptions.NoCacheLookupForPosts)]
     public class ControllerBase : Controller
     {
@@ -55,35 +58,163 @@ namespace InventoryManagementMVC.Controllers
             return Json(result.ToDataSourceResult(request));
         }
 
-        public ActionResult DestroyBase([DataSourceRequest] DataSourceRequest request,
-            [Bind(Prefix = "models")] IEnumerable<PaymentTypeViewModel> paymentTypes, Type modelType, Type entityType)
+        public ActionResult CreateBase([DataSourceRequest] DataSourceRequest request, [Bind(Prefix = "models")] IEnumerable<object> models, Type modelType, Type entityType)
         {
-            DbSet dbset = ContextFactory.Current.Set(entityType);
-            dbset.Load();
-            var en = dbset.Local.GetEnumerator();
-            
-            List<object> result = new List<object>();
-            while (en.MoveNext())
+            if (models != null && ModelState.IsValid)
             {
-                dynamic newModel = Activator.CreateInstance(modelType);
-                dynamic newEntity = Activator.CreateInstance(entityType);
-                newEntity = en.Current;
-                var modelToAdd = newModel.ConvertFromEntity(newEntity);
-                if (paymentTypes.Any(m => m == modelToAdd))
+                DbSet dbset = ContextFactory.Current.Set(entityType);
+
+                Hashtable entitiesHash = new Hashtable();
+                foreach (object model in models)
                 {
-                    result.Add(en.Current);
+                    dynamic newModel = model;
+                    dynamic newEntity = Activator.CreateInstance(entityType);
+
+                    newModel.ConvertToEntity(newEntity);
+
+                    dbset.Add(newEntity);
+
+                    entitiesHash.Add(newEntity, model);
+                }
+
+                ContextFactory.Current.SaveChanges();
+
+                dynamic dummyEntity = Activator.CreateInstance(entityType);
+                for (int i = 0; i < entitiesHash.Keys.Count; i++)
+                {
+                    dummyEntity = entitiesHash.Keys.Cast<object>().ElementAt(i);
+                    dynamic model = entitiesHash[dummyEntity];
+                    model.ConvertFromEntity(dummyEntity);
                 }
             }
 
-            foreach (var o in result)
+            return Json(models.ToDataSourceResult(request, ModelState));
+        }
+
+        public ActionResult UpdateBase([DataSourceRequest] DataSourceRequest request,
+            [Bind(Prefix = "models")] IEnumerable<object> models, Type modelType, Type entityType)
+        {
+            if (models != null && ModelState.IsValid)
             {
-                dbset.Remove(o);
+                List<object> entitiesToUpdate = GetEntitiesFromModels(models, modelType, entityType);
+
+                DbSet dbset = ContextFactory.Current.Set(entityType);
+
+                Hashtable entitiesHash = new Hashtable();
+
+                foreach (object entity in entitiesToUpdate)
+                {
+                    dynamic newModel = Activator.CreateInstance(modelType);
+                    dynamic newEntity = Activator.CreateInstance(entityType);
+                    newEntity = entity;
+                    var modelToAdd = newModel.ConvertFromEntity(newEntity);
+                    //dynamic model = models.FirstOrDefault(m => m == modelToAdd); // this fails because IEnumerable<object> models was List<PaymentTypeViewModel>
+                    dynamic model = models.FirstOrDefault(m => AreObjectsWithTheSameId(newEntity, m)); // this fails because IEnumerable<object> models was List<PaymentTypeViewModel>
+                    if (model != null)
+                    {
+                        model.ConvertToEntity(newEntity);
+                        entitiesHash.Add(newEntity, model);
+                    }
+                }
+
+                ContextFactory.Current.SaveChanges();
+
+                foreach (dynamic entity in entitiesToUpdate)
+                {
+                    dynamic model = entitiesHash[entity];
+                    model.ConvertFromEntity(entity); // Фиь тхис To Do нулл референце гере
+                }
+            }
+
+            return Json(models.ToDataSourceResult(request, ModelState));
+
+        }
+
+        private bool AreObjectsWithTheSameId(object entity, object model)
+        {
+            Type modelType = model.GetType();
+            var modelProps = modelType.GetProperties();
+            PropertyInfo modelKeyProperty = null;
+            foreach (var propertyInfo in modelProps)
+            {
+                KeyAttribute keyAttribute =
+                    propertyInfo.GetCustomAttributes<KeyAttribute>().FirstOrDefault();
+                if (keyAttribute != null)
+                {
+                    modelKeyProperty = propertyInfo;
+                    break;
+                }
+            }
+
+            Type entityType = entity.GetType();
+            var entityProps = entityType.GetProperties();
+            PropertyInfo entityKeyProperty = null;
+            foreach (var propertyInfo in entityProps)
+            {
+                if (propertyInfo.Name ==  modelKeyProperty.Name)
+                {
+                    entityKeyProperty = propertyInfo;
+                    break;
+                }
+            }
+
+            object modelKey = modelKeyProperty.GetValue(model);
+            object entityKey = entityKeyProperty.GetValue(entity);
+
+            if (modelKey.ToString() == entityKey.ToString())
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+
+        public ActionResult DestroyBase([DataSourceRequest] DataSourceRequest request,
+            [Bind(Prefix = "models")] IEnumerable<object> models, Type modelType, Type entityType)
+        {
+            List<object> entitiesToDelete = GetEntitiesFromModels(models, modelType, entityType);
+            DbSet dbset = ContextFactory.Current.Set(entityType);
+
+            foreach (var entityToDelete in entitiesToDelete)
+            {
+                dbset.Remove(entityToDelete);
                 // Do not call  ContextFactory.Current.SaveChanges(); here !!! It will delete only the first element!
-            } 
+            }
             ContextFactory.Current.SaveChanges();
 
-            return Json(paymentTypes.ToDataSourceResult(request, ModelState));
+            return Json(models.ToDataSourceResult(request, ModelState));
         }
-    
+
+        private List<object> GetEntitiesFromModels(IEnumerable<object> models, Type modelType, Type entityType)
+        {
+            List<object> entities = new List<object>();
+
+            var props = modelType.GetProperties();
+            PropertyInfo keyProperty = null;
+            foreach (var propertyInfo in props)
+            {
+                KeyAttribute keyAttribute =
+                       propertyInfo.GetCustomAttributes<KeyAttribute>().FirstOrDefault();
+                if (keyAttribute != null)
+                {
+                    keyProperty = propertyInfo;
+                    break;
+                }
+            }
+
+            DbSet dbset = ContextFactory.Current.Set(entityType);
+            foreach (object model in models)
+            {
+                object key = keyProperty.GetValue(model);
+
+                object entity = dbset.Find(new[] { key });
+                entities.Add(entity);
+            }
+
+            return entities;
+        }
+
+
     }
 }
