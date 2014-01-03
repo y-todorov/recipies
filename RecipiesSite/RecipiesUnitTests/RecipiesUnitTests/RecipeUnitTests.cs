6f029f23440172dcf9ef0610267eb1ab02b6aa4b
y-todorov <ytodorov@ytodorov.com>
2014-01-03 23:53:29 +0200

    added test project to the solution

---
+++ RecipiesSite/RecipiesUnitTests/RecipiesUnitTests/RecipeUnitTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RecipiesModelNS;
+using System.Collections.Generic;
+
+
+namespace RecipiesUnitTests
+{
+    //[Ignore()] // These test are tun in the cloud and deployment fails if a test fails.
+    [TestClass]
+    public class RecipeUnitTests
+    {
+        [TestMethod]
+        public void FindAllProductsWithQuantitiesInRecipe()
+        {
+            Recipe aRecipie = ContextFactory.Current.Recipes.FirstOrDefault();
+            Dictionary<int, double> products = new Dictionary<int, double>();
+            Recipe.GetProductsWithQuantities(aRecipie.RecipeId, products);
+            Assert.AreNotEqual(0, products.Count);
+        }
+    }
+}
