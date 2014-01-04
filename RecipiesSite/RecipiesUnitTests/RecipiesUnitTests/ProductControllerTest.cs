c7c9c88b3a49d848575769c2e35490f9d13f2879
y-todorov <ytodorov@ytodorov.com>
2014-01-04 20:21:03 +0200

    ProductControllerTests CRUD completed

--- RecipiesSite/RecipiesUnitTests/RecipiesUnitTests/ProductControllerTest.cs
+++ RecipiesSite/RecipiesUnitTests/RecipiesUnitTests/ProductControllerTest.cs
@@ -15,10 +15,10 @@ namespace RecipiesUnitTests
     public class ProductControllerTest
     {
 
-//[ClassInitialize()]   Use ClassInitialize to run code before you run the first test in the class.
-//[ClassCleanUp()]   Use ClassCleanup to run code after all tests in a class have run.
-//[TestInitialize()]   Use TestInitialize to run code before you run each test.
-//[TestCleanUp()]   Use TestCleanup to run code after each test has run.
+        //[ClassInitialize()]   Use ClassInitialize to run code before you run the first test in the class.
+        //[ClassCleanUp()]   Use ClassCleanup to run code after all tests in a class have run.
+        //[TestInitialize()]   Use TestInitialize to run code before you run each test.
+        //[TestCleanUp()]   Use TestCleanup to run code after each test has run.
 
         ProductController productController;
         DataSourceRequest request;
@@ -43,7 +43,7 @@ namespace RecipiesUnitTests
             // Arrange
             ProductViewModel newProduct = new ProductViewModel();
             UnitTestHelper.InitializeModelWithRandomValues(newProduct);
-            List<ProductViewModel> products = new List<ProductViewModel>() {newProduct};
+            List<ProductViewModel> products = new List<ProductViewModel>() { newProduct };
             JsonResult jr = productController.Create(request, products) as JsonResult;
             // Act
             ProductViewModel savedProduct = (jr.Data as DataSourceResult).Data.Cast<ProductViewModel>().FirstOrDefault();
@@ -63,9 +63,51 @@ namespace RecipiesUnitTests
             Assert.AreEqual(resultProducts.Count, ContextFactory.Current.Products.Count());
         }
 
+        [TestMethod]
         public void UpdateTest()
         {
-            //productController.Update()
+            // Arrange
+            JsonResult jrr = productController.Read(request) as JsonResult;
+
+            // Act
+            ProductViewModel existingProduct = (jrr.Data as DataSourceResult).Data.Cast<ProductViewModel>().FirstOrDefault();
+
+            UnitTestHelper.InitializeModelWithRandomValues(existingProduct);
+
+            List<ProductViewModel> productsForUpdate = new List<ProductViewModel>() { existingProduct };
+
+
+            JsonResult jru = productController.Update(request, productsForUpdate) as JsonResult;
+            ProductViewModel savedProduct = (jru.Data as DataSourceResult).Data.Cast<ProductViewModel>().FirstOrDefault();
+
+
+            bool areEqual = UnitTestHelper.CheckIfTwoModelsAreEqual(existingProduct, savedProduct);
+            areEqual &= existingProduct.ProductId == savedProduct.ProductId;
+
+            // Assert
+            Assert.IsTrue(areEqual, "Saved product field values are not equalt to values that are to be saved!");
+        }
+
+        [TestMethod]
+        public void DeleteTest()
+        {
+            // Arrange
+            JsonResult jrr = productController.Read(request) as JsonResult;
+
+            List<ProductViewModel> allProducts = (jrr.Data as DataSourceResult).Data.Cast<ProductViewModel>().ToList();
+
+            // Act
+            ProductViewModel existingProduct = (jrr.Data as DataSourceResult).Data.Cast<ProductViewModel>().FirstOrDefault();
+
+            List<ProductViewModel> productsForDelete = new List<ProductViewModel>() { existingProduct };
+
+            JsonResult jru = productController.Destroy(request, productsForDelete) as JsonResult;
+            ProductViewModel deletedProduct = (jru.Data as DataSourceResult).Data.Cast<ProductViewModel>().FirstOrDefault();
+
+            JsonResult jrAfterDelete = productController.Read(request) as JsonResult;
+            List<ProductViewModel> allProductsAfterDelete = (jrAfterDelete.Data as DataSourceResult).Data.Cast<ProductViewModel>().ToList();
+
+            Assert.AreEqual(allProducts.Count - 1, allProductsAfterDelete.Count);
         }
     }
 }
