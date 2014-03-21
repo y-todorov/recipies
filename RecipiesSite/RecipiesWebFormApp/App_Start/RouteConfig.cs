7f9cbddceaaced0c02ac15e837196b13d34c57f3
y-todorov <ytodorov@ytodorov.com>
2014-03-21 21:11:18 +0200

    remove some old files

--- RecipiesSite/RecipiesWebFormApp/App_Start/RouteConfig.cs
+++ RecipiesSite/RecipiesWebFormApp/App_Start/RouteConfig.cs
@@ -8,11 +8,6 @@ namespace RecipiesWebFormApp
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            //var settings = new FriendlyUrlSettings();
-            //settings.AutoRedirectMode = RedirectMode.Permanent;
-            //routes.EnableFriendlyUrls(settings);
-
-
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
             routes.IgnoreRoute("{resource}.aspx/{*pathInfo}");
             routes.MapRoute(
