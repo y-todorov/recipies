2fc34060553eb42b321bd624d8816427907d9ffc
y-todorov <ytodorov@ytodorov.com>
2014-03-30 21:33:14 +0300

    remove  error handler in global.asax

--- RecipiesSite/RecipiesWebFormApp/Global.asax.cs
+++ RecipiesSite/RecipiesWebFormApp/Global.asax.cs
@@ -56,76 +56,76 @@ namespace RecipiesWebFormApp
 
         private void Application_Error(object sender, EventArgs e)
         {
-            var httpContext = ((HttpApplication) sender).Context;
-            var currentController = " ";
-            var currentAction = " ";
-            var currentRouteData = RouteTable.Routes.GetRouteData(new HttpContextWrapper(httpContext));
+//            var httpContext = ((HttpApplication) sender).Context;
+//            var currentController = " ";
+//            var currentAction = " ";
+//            var currentRouteData = RouteTable.Routes.GetRouteData(new HttpContextWrapper(httpContext));
 
-            if (currentRouteData != null)
-            {
-                if (currentRouteData.Values["controller"] != null &&
-                    !String.IsNullOrEmpty(currentRouteData.Values["controller"].ToString()))
-                {
-                    currentController = currentRouteData.Values["controller"].ToString();
-                }
+//            if (currentRouteData != null)
+//            {
+//                if (currentRouteData.Values["controller"] != null &&
+//                    !String.IsNullOrEmpty(currentRouteData.Values["controller"].ToString()))
+//                {
+//                    currentController = currentRouteData.Values["controller"].ToString();
+//                }
 
-                if (currentRouteData.Values["action"] != null &&
-                    !String.IsNullOrEmpty(currentRouteData.Values["action"].ToString()))
-                {
-                    currentAction = currentRouteData.Values["action"].ToString();
-                }
-            }
+//                if (currentRouteData.Values["action"] != null &&
+//                    !String.IsNullOrEmpty(currentRouteData.Values["action"].ToString()))
+//                {
+//                    currentAction = currentRouteData.Values["action"].ToString();
+//                }
+//            }
 
-            var ex = Server.GetLastError();
-            var controller = new ErrorController();
-            var routeData = new RouteData();
-            var action = "Index";
+//            var ex = Server.GetLastError();
+//            var controller = new ErrorController();
+//            var routeData = new RouteData();
+//            var action = "Index";
 
-            if (ex is HttpException)
-            {
-                var httpEx = ex as HttpException;
-                int exceptionHttpStatusCode = httpEx.GetHttpCode();
-                switch (exceptionHttpStatusCode)
-                {
-                    case 404:
-                        action = "NotFound";
-                        break;
-                    case 500:
-                        action = "InternalServerError";
-                        break;
-                        // others if any
-                }
-            }
+//            if (ex is HttpException)
+//            {
+//                var httpEx = ex as HttpException;
+//                int exceptionHttpStatusCode = httpEx.GetHttpCode();
+//                switch (exceptionHttpStatusCode)
+//                {
+//                    case 404:
+//                        action = "NotFound";
+//                        break;
+//                    case 500:
+//                        action = "InternalServerError";
+//                        break;
+//                        // others if any
+//                }
+//            }
 
-            //test
+//            //test
 
-            if (IsAjaxRequest())
-            {
-                Response.Write("Your JSON here");
-                //We clear the error
-                httpContext.Response.StatusCode = 500;
-                Server.ClearError();
-                return;
-            }
+//            if (IsAjaxRequest())
+//            {
+//                Response.Write("Your JSON here");
+//                //We clear the error
+//                httpContext.Response.StatusCode = 500;
+//                Server.ClearError();
+//                return;
+//            }
 
-            httpContext.ClearError();
-            httpContext.Response.Clear();
-            httpContext.Response.StatusCode = ex is HttpException ? ((HttpException) ex).GetHttpCode() : 500;
-            //but this is importan. It helps the brousers to display a good error page
-            httpContext.Response.StatusCode = 200;
+//            httpContext.ClearError();
+//            httpContext.Response.Clear();
+//            httpContext.Response.StatusCode = ex is HttpException ? ((HttpException) ex).GetHttpCode() : 500;
+//            //but this is importan. It helps the brousers to display a good error page
+//            httpContext.Response.StatusCode = 200;
 
-            httpContext.Response.TrySkipIisCustomErrors = true;
+//            httpContext.Response.TrySkipIisCustomErrors = true;
 
-            routeData.Values["controller"] = "Error";
-            routeData.Values["action"] = action;
+//            routeData.Values["controller"] = "Error";
+//            routeData.Values["action"] = action;
 
-            controller.ViewData.Model = new HandleErrorInfo(ex, currentController, currentAction);
-            ((IController) controller).Execute(new RequestContext(new HttpContextWrapper(httpContext), routeData));
+//            controller.ViewData.Model = new HandleErrorInfo(ex, currentController, currentAction);
+//            ((IController) controller).Execute(new RequestContext(new HttpContextWrapper(httpContext), routeData));
 
-#if DEBUG
-            return;
-#endif
-            LogentriesHelper.ApplicationLog.Error("Application_Error", ex);
+//#if DEBUG
+//            return;
+//#endif
+//            LogentriesHelper.ApplicationLog.Error("Application_Error", ex);
 
         }
 
